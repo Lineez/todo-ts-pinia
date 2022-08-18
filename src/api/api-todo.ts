bdee8166a0d377368d95b8f82b09aad3ac643980
Lineez <qewqew26@gmail.com>
2022-08-18 11:39:34 +0300

    add type for api data params & update api constructor

--- src/api/api-todo.ts
+++ src/api/api-todo.ts
@@ -1,4 +1,5 @@
 import { API_Constructor } from "@/api/constructor";
+import { ITodoItem } from "@/components/todo/interface.todo";
 const API_URL = new URL(process.env.VUE_APP_API_TODO);
 
-export const ApiTodo = new API_Constructor(API_URL);
+export const ApiTodo = new API_Constructor<ITodoItem>(API_URL);
